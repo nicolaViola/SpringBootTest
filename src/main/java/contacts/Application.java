0d69b312267883a882c75a8eeb64bdc23f5c5a6c
Viola_N <nicola.viola@regione.emilia-romagna.it>
2019-02-06 16:31:39 +0100

    disabilitata la security

--- src/main/java/contacts/Application.java
+++ src/main/java/contacts/Application.java
@@ -3,17 +3,10 @@ package contacts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication(scanBasePackages = {"contacts"})
 @EnableConfigurationProperties(AppSettings.class)
-public class Application extends WebSecurityConfigurerAdapter{
+public class Application /*extends WebSecurityConfigurerAdapter*/{
 	
 
 	public static void main(String[] args) {
@@ -22,26 +15,26 @@ public class Application extends WebSecurityConfigurerAdapter{
 		
 	}
 	
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-		  
-		  http.mvcMatcher("/**").authorizeRequests()
-		  .anyRequest().hasRole("ADMIN")
-	      .and()
-	      .httpBasic();
-	  }
-	  
-	  @Bean
-	  @Override
-	  public UserDetailsService userDetailsService() {
-	      UserDetails user =
-	          User.withDefaultPasswordEncoder()
-	              .username("nicola")
-	              .password("viola")
-	              .roles("ADMIN")
-	              .build();
-	      return new InMemoryUserDetailsManager(user);
-
-	  }
+//	  @Override
+//	  protected void configure(HttpSecurity http) throws Exception {
+//		  
+//		  http.mvcMatcher("/**").authorizeRequests()
+//		  .anyRequest().hasRole("ADMIN")
+//	      .and()
+//	      .httpBasic();
+//	  }
+//	  
+//	  @Bean
+//	  @Override
+//	  public UserDetailsService userDetailsService() {
+//	      UserDetails user =
+//	          User.withDefaultPasswordEncoder()
+//	              .username("nicola")
+//	              .password("viola")
+//	              .roles("ADMIN")
+//	              .build();
+//	      return new InMemoryUserDetailsManager(user);
+//
+//	  }
 	
 }
