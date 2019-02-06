9b8a7e7cf35594971caf02639fffcbe0a6a62137
Viola_N <nicola.viola@regione.emilia-romagna.it>
2019-02-06 15:16:21 +0100

    refactoring

--- src/main/java/contacts/Application.java
+++ src/main/java/contacts/Application.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-//@RestController
 @SpringBootApplication(scanBasePackages = {"contacts"})
 @EnableConfigurationProperties(AppSettings.class)
 public class Application extends WebSecurityConfigurerAdapter{
@@ -30,11 +29,6 @@ public class Application extends WebSecurityConfigurerAdapter{
 		  .anyRequest().hasRole("ADMIN")
 	      .and()
 	      .httpBasic();
-		  
-//	    http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
-//	      .anyRequest().hasRole("ADMIN")
-//	      .and()
-//	      .httpBasic();
 	  }
 	  
 	  @Bean
@@ -50,69 +44,4 @@ public class Application extends WebSecurityConfigurerAdapter{
 
 	  }
 	
-//	@Bean
-//	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-//	    return http.authorizeExchange()
-//	      .pathMatchers("/actuator/**").permitAll()
-//	      .anyExchange().authenticated()
-//	      .and().build();
-//	}
-	
-	
-	
-//	@RequestMapping(path="/")
-//	@GetMapping
-//	public String ccc(){
-//		//return applicationContext.getBeanDefinitionNames();
-//		
-//		StringBuilder sb = new StringBuilder("<html><body>");
-//		sb.append("Hello there dear developer, here are the beans you were looking for: </br>");
-//		//method that returns all the bean names in the context of the application
-//		String beanNames[] = applicationContext.getBeanDefinitionNames();
-//		Arrays.sort(beanNames);
-//		
-//		for (String beanName : beanNames) {
-//			sb.append("</br>").append(beanName);
-//		}
-//		sb.append("</body></htm>");
-//		return sb.toString();
-//		
-//	}
-	
-//	@Value("${app.context}")
-//	private String contextPath;
-//	
-//	@Bean
-//	public TomcatServletWebServerFactory containerFactory() {
-//		TomcatServletWebServerFactory xc =  new TomcatServletWebServerFactory() {
-//            protected void customizeConnector(Connector connector) {
-//                int maxSize = 50000000;
-//                super.customizeConnector(connector);
-//                connector.setMaxPostSize(maxSize);
-//                connector.setMaxSavePostSize(maxSize);
-//                if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
-//
-//                    ((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(maxSize);
-//                    logger.info("Set MaxSwallowSize "+ maxSize);
-//                }
-//            }
-//        };
-//        xc.setContextPath(contextPath);
-//        return xc;
-//    }
-	
-	
-//	@Bean
-//	public EmbeddedServletContainerFactory servletContainer() {
-//	TomcatEmbeddedServletContainerFactory factory =
-//	new TomcatEmbeddedServletContainerFactory();
-//	factory.setPort(value);
-//	factory.setSessionTimeout(10, TimeUnit.MINUTES);
-//	factory.setContextPath(contextPath);
-//	return factory;
-//	}
-	
-
-	
-
 }
